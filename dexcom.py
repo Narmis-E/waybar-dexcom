eea48e1a49970f8563c4ef46debd0936a85b6d42
narmis <109248529+Narmis-E@users.noreply.github.com>
2025-01-27 15:30:57 +1030

    Updated dexcom.py to reflect changes in readme

--- dexcom.py
+++ dexcom.py
@@ -7,12 +7,17 @@ def calculate_delta(current_bg, previous_bg):
 
 dexcom = Dexcom("username", "password", ous=True)
 
-latest_readings = dexcom.get_glucose_readings(minutes=10, max_count=2)
-current_bg_value = latest_readings[0].mmol_l
-previous_bg_value = latest_readings[1].mmol_l
+try:
+    latest_readings = dexcom.get_glucose_readings(minutes=10, max_count=2)
+    if len(latest_readings) >= 2:
+        current_bg_value = latest_readings[0].mmol_l
+        previous_bg_value = latest_readings[1].mmol_l
 
-delta = calculate_delta(current_bg_value, previous_bg_value)
-delta_str = f"{'' if delta == 0 else ('+' if delta > 0 else '-')} {abs(delta):.1f}"
-delta_str = delta_str.replace(" ", "")
+        delta = calculate_delta(current_bg_value, previous_bg_value)
+        delta_str = f"{'' if delta == 0 else ('+' if delta > 0 else '-')} {abs(delta):.1f}".replace(" ", "")
 
-print(f'{{"text":"{current_bg_value:.1f} {latest_readings[0].trend_arrow}", "tooltip":"{delta_str}"}}')
+        print(f'{{"text":"{current_bg_value:.1f} {latest_readings[0].trend_arrow}", "tooltip":"{delta_str}"}}')
+    else:
+        print('{"text":"---", "tooltip":"No readings available"}')
+except Exception as e:
+    print('{"text":"---", "tooltip":"Error retrieving data"}')
